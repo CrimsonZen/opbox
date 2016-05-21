86ab6be7ac63d061f212a5343bfae1502f72511b
Conrad Stansbury <chstansbury@gmail.com>
2016-05-20 20:28:34 -0700

    Let StanceBox handle styling through props, placeholder image

--- src/StanceBox.jsx
+++ src/StanceBox.jsx
@@ -1,21 +1,27 @@
 /**
- * Created by Chris on 5/20/2016.
- */
+* Created by Chris on 5/20/2016.
+*/
 import React from "react";
 
 class StanceBox extends React.Component {
-    // current state - reference to state object + meta-state
-    //
-    render () {
-        return (
-            <text>{this.props.stance.name}</text>
-        );
-    }
-    static propTypes() {
-        return {
-            stance: React.PropTypes.object.required
-        }
-    }
+  // current state - reference to state object + meta-state
+  //
+  render () {
+    const fontStyle = this.props.selected ? "bold" : "normal";
+    const picture = this.props.stance.picture || "https://placehold.it/150x150";
+    return (
+      <g>
+        <image href={picture} width="150px" height="150px"/>
+        <text style={{ fontWeight: fontStyle }}>{this.props.stance.name}</text>
+      </g>
+    );
+  }
+  static propTypes() {
+    return {
+      stance: React.PropTypes.object.required,
+      selected: React.PropTypes.bool.required,
+    };
+  }
 }
 
 export default StanceBox;
